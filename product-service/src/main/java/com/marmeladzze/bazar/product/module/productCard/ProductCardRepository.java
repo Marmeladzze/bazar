d0f6170ea79dc816bcf9201985c6df1398a580c8
Marmeladze <magomedov.ramazan2009@mail.ru>
2026-06-04 20:54:31 +0300

    feat/productCard-entity-repository

---
+++ product-service/src/main/java/com/marmeladzze/bazar/product/module/productCard/ProductCardRepository.java
@@ -0,0 +1,7 @@
+package com.marmeladzze.bazar.product.module.productCard;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductCardRepository extends JpaRepository {}
